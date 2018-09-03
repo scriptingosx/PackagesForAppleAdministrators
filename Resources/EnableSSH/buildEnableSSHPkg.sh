8fc23d6491f78b69bc14defe50e376852e71562d
Armin Briegel <ab@scriptingosx.com>
2018-09-03 14:57:25 +0200

    added EnableSSH code

---
+++ Resources/EnableSSH/buildEnableSSHPkg.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+pkgname="EnableSSH"
+version="1.0"
+identifier="com.example.${pkgname}"
+
+PATH=/usr/bin:/bin:/usr/sbin:/sbin export PATH
+
+projectfolder=$(dirname "$0")
+
+pkgbuild --nopayload \
+         --identifier "${identifier}" \
+         --version "${version}" \
+         --scripts "${projectfolder}/scripts" \
+         "${projectfolder}/${pkgname}-${version}.pkg"
